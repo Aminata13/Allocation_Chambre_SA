0b8f13dc77c5760de8fc195539b3511c41b85a9b
Aminata13 <59060784+Aminata13@users.noreply.github.com>
2020-06-25 15:32:09 +0000

    navigation

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 define('BASE_URL', 'http://localhost/allocation_chambre_sa/');
 
 
-<<<<<<< HEAD
-// $router = new Router();
-// $router->route();
-echo "je teste";
-=======
 require_once('./lib/Router.php');
 
 $router = new Router();
 $router->route();
->>>>>>> origin/master
